b96cca270c224ce12a54adfdf2f35d07933e00a8
eduardobnhr <eduardobnhr@gmail.com>
2024-04-01 08:13:23 -0400

    create java project using spring kafka

---
+++ src/main/java/br/com/eduardo/picpaydesafiobackend/transaction/Transaction.java
@@ -0,0 +1,7 @@
+package br.com.eduardo.picpaydesafiobackend.transaction;
+
+public record Transaction(
+    @Id
+) {
+    
+}
---
+++ src/test/java/br/com/eduardo/picpaydesafiobackend/PicpayDesafioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eduardo.picpaydesafiobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayDesafioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
